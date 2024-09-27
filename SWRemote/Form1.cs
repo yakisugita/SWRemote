b38c76258e9b408e101523f8e947e7032afe7988
yakisugita <yakisugita@gmail.com>
2024-09-27 18:15:20 +0900

    Grapevine導入,サンプルコード動作確認

--- SWRemote/Form1.cs
+++ SWRemote/Form1.cs
@@ -1,3 +1,7 @@
+using Grapevine;
+using System.Diagnostics;
+using System.Net.Http;
+
 namespace SWRemote
 {
     public partial class Form1 : Form
@@ -5,6 +9,31 @@ namespace SWRemote
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("a");
+            MessageBox.Show("テキスト", "デバッグ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            var server = RestServerBuilder.UseDefaults().Build();
+            server.Start();
+        }
+    }
+
+    [RestResource]
+    public class MyResource
+    {
+        [RestRoute("Get", "/api/test")]
+        public async Task Test(IHttpContext context)
+        {
+            Debug.WriteLine("ACCESS");
+            await context.Response.SendResponseAsync("Successfully hit the test route!").ConfigureAwait(false);
         }
     }
 }
