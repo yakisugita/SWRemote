53075e6a0a47ecf09be9cf3650156e922ae04ae3
yakisugita <yakisugita@gmail.com>
2024-09-29 20:51:52 +0900

    一時保存用クラス名変更,ブラウザからの値送信テスト

--- SWRemote/Form1.cs
+++ SWRemote/Form1.cs
@@ -2,6 +2,7 @@ using Grapevine;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SWRemote
 {
@@ -20,7 +21,7 @@ namespace SWRemote
         private void button1_Click(object sender, EventArgs e)
         {
             //MessageBox.Show("テキスト", "デバッグ", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            Settings.count = 0;
+            ValueStore.count = 0;
 
             var server = RestServerBuilder.UseDefaults().Build();
             //デフォルトだとlocalhost意外は弾く Stormworksは127.0.0.1でアクセスする
@@ -38,15 +39,27 @@ namespace SWRemote
         public async Task Test(IHttpContext context)
         {
             Debug.WriteLine("ACCESS");
-            Settings.count = Settings.count + 1;
-            Debug.WriteLine(Settings.count);
+
+            for (int i = 0; i < context.Request.QueryString.AllKeys.Length; i++)
+            {
+                switch (context.Request.QueryString.AllKeys[i])
+                {
+                    case "numAD":
+                        int.TryParse(context.Request.QueryString.GetValue<string>(context.Request.QueryString.AllKeys[i]), out ValueStore.NumAD);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Debug.WriteLine(ValueStore.NumAD.ToString());
             //await context.Response.SendResponseAsync("Successfully hit the test route!").ConfigureAwait(false);
-            await context.Response.SendResponseAsync("Welcome!"+Settings.count.ToString()).ConfigureAwait(false);
+            await context.Response.SendResponseAsync("Welcome!"+ValueStore.count.ToString()).ConfigureAwait(false);
         }
     }
 
-    public static class Settings
+    public static class ValueStore
     {
         public static int count;
+        public static int NumAD = 0;
     }
 }
--- SWRemote/website/main.js
+++ SWRemote/website/main.js
@@ -1,1 +1,18 @@
-alert("test")
+setInterval(() => {
+    const searchQuery = {
+        "numAD" : document.getElementById("slider").value,
+    }
+    
+    const urlSearchParam = new URLSearchParams(searchQuery).toString()
+    
+    const request = new XMLHttpRequest()
+    request.open("GET", `/api/test?${urlSearchParam}`, false)
+    
+    request.send()
+    
+    if (request.status == 0) console.log("エラー")
+    if (request.status != 200) console.log("NOT200")
+    console.log(request.responseText)
+    //const searchResult = JSON.parse(request.responseText);
+    //console.log(searchResult)
+}, 500);
